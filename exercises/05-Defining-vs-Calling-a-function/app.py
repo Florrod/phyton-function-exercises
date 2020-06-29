f87ef5a401fb13b0fe2ceaf939137ccc2aa64138
Flor Rodriguez <florrod23@gmail.com>
2020-06-29 19:11:01 +0000

    Ejercicios completos

--- exercises/05-Defining-vs-Calling-a-function/app.py
+++ exercises/05-Defining-vs-Calling-a-function/app.py
@@ -1,5 +1,6 @@
 # Define the function called "multi" that expects 2 parameters:
-
+def multi(value_1,value_2):
+    return value_1*value_2
 # don't edit anything below this line
 return_value = multi(7,53812212)
 print(return_value)
--- exercises/09-Function-parameters/app.py
+++ exercises/09-Function-parameters/app.py
@@ -1,7 +1,10 @@
 # Your code goes here:
-def render_person(param):
-    return param
+def render_person(name, birthdate, eyecolor, age, gender):
+    return name+" is a "+ str(age)+" years old "+ gender +" born in "+ birthdate +" with "+ eyecolor +" eyes"
+
 
 
 # Do not edit below this line
 print(render_person('Bob', '05/22/1983', 'green', 23, 'male'))
+
+# Your code goes here:
